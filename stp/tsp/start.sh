d6970c68b506a3a22def59222b304adf1ea40af1
wazed ali <mdwazed@gmail.com>
2019-12-24 09:17:34 +0600

    start script updated

--- stp/tsp/start.sh
+++ stp/tsp/start.sh
@@ -3,7 +3,7 @@
 echo "starting djnago"
 python manage.py migrate
 echo "migration complete"
-python manage.py collectstatic
+echo "yes" | python manage.py collectstatic
 echo "collect static complete"
 # python manage.py runserver 0.0.0.0:8000
 uwsgi --http :8000 --module tsp.wsgi
