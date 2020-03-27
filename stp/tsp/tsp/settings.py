ce38dc1545f6d9a5d9ac85ad192f46e4aae71b49
wazed ali <mdwazed@gmail.com>
2020-03-27 21:44:37 +0600

    trying remote dak

--- stp/tsp/tsp/settings.py
+++ stp/tsp/tsp/settings.py
@@ -186,8 +186,8 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 # Domain of all sta for receiving remote dak
 DOMAINS = {
-    'DHK':'192.168.13.102:8000',
-    'JSR':'192.168.13.102:8000',
+    'DHK':'192.168.13.102:8006',
+    'JSR':'192.168.13.102:8006',
 }
 
 
